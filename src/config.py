e8c8a00597233cf1c1138446f12dffaf765fce6e
xqz-u <marco.gallo0530@gmail.com>
2023-01-18 18:11:01 +0100

    download the datasets locally from datasets.py so that they don't clog the repo

--- src/config.py
+++ src/config.py
@@ -6,7 +6,10 @@ CODE_DIR = ROOT_DIR.parent
 
 DATA_DIR = ROOT_DIR / "data"
 LASTFM_DIR = DATA_DIR / "Lastfm"
-MOVIELENS_DIR = DATA_DIR / "MovieLens"
+MOVIELENS_DIR = DATA_DIR / "MovieLens" / "ml-1m"
+
+LASTFM_URL = "https://files.grouplens.org/datasets/hetrec2011/hetrec2011-lastfm-2k.zip"
+MOVIELENS_URL = "https://files.grouplens.org/datasets/movielens/ml-1m.zip"
 
 MODELS_DIR = ROOT_DIR / "models"
 MODELS_DIR.mkdir(exist_ok=True)
