235be0c97df66216ef476d12e892df72482f962a
xqz-u <marco.gallo0530@gmail.com>
2023-01-18 13:38:20 +0000

    movielens dataset + preproc, TODO train recommenders

--- src/config.py
+++ src/config.py
@@ -6,6 +6,7 @@ CODE_DIR = ROOT_DIR.parent
 
 DATA_DIR = ROOT_DIR / "data"
 LASTFM_DIR = DATA_DIR / "Lastfm"
+MOVIELENS_DIR = DATA_DIR / "MovieLens"
 
 MODELS_DIR = ROOT_DIR / "models"
 MODELS_DIR.mkdir(exist_ok=True)
