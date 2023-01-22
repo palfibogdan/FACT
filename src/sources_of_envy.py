12cb0ae7fe1627497de311b08c9f9faa7832c601
palfibogdan <64193192+palfibogdan@users.noreply.github.com>
2023-01-22 15:43:37 +0100

    Running experiment 5.1.1 function

--- src/sources_of_envy.py
+++ src/sources_of_envy.py
@@ -1,66 +1,40 @@
 import numpy as np
-
-# TODO need to find way to get utilities from the data
-
-
-def get_utilities():
-    pass
+import config
+import rl
+import constants
 
 
-def get_delta(m, M, utilities_m):
-
-    max_utility = 0
-    for user in M:
-        if user != m and utilities_m[user] > max_utility:
-            max_utility = utilities_m[user]
-
-    return max(max_utility - utilities_m[m], 0)
+def recommendation_probs(
+    user_preferences: np.ndarray, temperature: float
+) -> np.ndarray:
+    return scipy.special.softmax(user_preferences / temperature, axis=1)
 
 
-def get_average_envy(M, utilities):
-    avg_envy = 0
-    for user in M:
-        avg_envy += get_delta(user, M, utilities)
-
-    return avg_envy / len(M)
-
-
-def get_user_proportion(M, eps, utilities):
-    user_proportion = 0
-
-    for user in M:
-        if get_delta(user, M, utilities) > eps:
-            user_proportion += 1
-
-    return user_proportion / len(M)
-
-
-def degree_of_envy(user_recommendation: np.ndarray, user_reward: np.ndarray, artists_not_recommended: np.ndarray) -> np.ndarray:
-    # get the index of the recommended item for this user
-    recommended_item_idx = np.argmax(user_recommendation)
-    degree_of_envy = max(np.ma.masked_array(
-        user_reward, artists_not_recommended) - user_reward[recommended_item_idx])
-    return degree_of_envy
+# NOTE works with both the full user-item preferences matrix and a single user's
+# preferences, add a batch dimension in the latter case
+def user_policy_recommendation(
+    user_preferences: np.ndarray, temperature: float, rng: np.random.Generator
+) -> np.ndarray:
+    # apply softmax with temperature to turn preferences into probabilities
+    probs = recommendation_probs(user_preferences, temperature)
+    # retrieve the indexes of the most recommended item for each user according to the
+    # softmax probabilities
+    # NOTE the vectorized version that samples over the whole matrix returns the
+    # same sample for each user :/
+    recommended_items_indexes = np.array(
+        [
+            rng.choice(user_preferences.shape[1], replace=False, p=user_probs)
+            for user_probs in recommendation_probs
+        ]
+    )
+    return recommended_items_indexes
 
 
-def degrees_of_envy(rewards_mat: np.ndarray, recommendations_mat: np.ndarray) -> np.ndarray:
-    # get items never recommended (so never envied by any user)
-    artists_not_recommended = recommendations_mat.sum(axis=0) == 0
-    degrees_of_envy = []
-    for user_index in range(rewards_mat.shape[0]):
-        user_envy = degree_of_envy(
-            recommendations_mat[user_index], rewards_mat[user_index], artists_not_recommended)
-        degrees_of_envy.append(user_envy)
-    return np.array(degrees_of_envy)
-
-
-def get_utilities(ground_truth_preferences, recommendations):
+def get_utilities(expected_rewards, item_probabilities):
     # return a utility matrix where element i, j  represents
     # the utility of user i with the policies j
-    utilities = ground_truth_preferences @ recommendations.T
+    utilities = item_probabilities @ expected_rewards.T
     return utilities
-    
-# [u^m(pi^m), u^m(pi^n1), u^m(pi^n2), u^m(pi^n3), u^m(pi^n4), u^m(pi^n5)]
 
 
 def get_deltas(M, utilities):
@@ -79,12 +53,56 @@ def get_deltas(M, utilities):
 
 def get_average_proportion(M, utilities, eps):
     """
-	M - set of users {m, n1, n2, ...}
-	Utilities - matrix - utilities for each user for each policy
-	eps - scalar
-	"""
+        M - set of users {m, n1, n2, ...}
+        Utilities - matrix - utilities for each user for each policy
+        eps - scalar
+        """
     deltas = get_deltas(M, utilities)
 
     return np.mean(deltas), np.mean(deltas > eps)
 
-# row of utility matrix u^m [m: 0.1, n1: 0.2, n2: 0.3, n4: 0.4, n5: 0.5]
+
+def run_experiment_5_1_1(filenames, eps, num_users):
+    """
+    Loads trained recommendation system models, gets the utility matrix given by these models
+    and calculates the average envy and the proportion of eps-envious users
+    Return list of average envy and proportion of eps-envious users for each number of factors
+    """
+    
+    # Lists for storing the average envy and proportion of eps-envious users for each number of factors
+    avg_envy_list = []
+    prop_envy_users_list = []
+
+    temperature = 5
+    rng = np.random.default_rng(constants.SEED)
+    ground_truth_lastfm = np.load(config.MODELS_DIR / "ground_truth_lastfm.npy")
+
+    # set of users
+    M = np.arrange(num_users)
+
+    for filename in filenames:
+
+        # load recommender system model
+        recommender = np.load(config.MODELS_DIR / filename)
+
+        # 2D: recommendation scores for each item per each user
+        recommender_preferences = recommender.user_preferences @ recommender.item_preferences.T
+
+        # get indexes of recommended items given by the model
+        recommendations = rl.user_policy_recommendation(recommender_preferences, temperature, rng)
+
+        # 2D: for each user, what items are truly recommended
+        true_recommendations = rl.generate_true_recommendation(ground_truth_lastfm, temperature, rng)
+
+        #TODO fix this: expected rewards are 1D array, but true_recommendations is 2D
+        #TODO See how to get these two variables
+        expected_rewards = np.take_along_axis(true_recommendations, recommendations[:, None], axis=1).squeeze()
+        item_probabilities = recommendation_probs(recommender_preferences, temperature)
+
+        utility_matrix = get_utilities(expected_rewards, item_probabilities)
+        average_envy, prop_envy_users = get_average_proportion(M, utility_matrix, eps)
+
+        avg_envy_list.append(average_envy)
+        prop_envy_users_list.append(prop_envy_users)
+
+    return avg_envy_list, prop_envy_users_list
